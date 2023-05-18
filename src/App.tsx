476b10c96e3391311b15bdbd0922aa8e1c032a90
Santiagokmids <sntgtrochez@gmail.com>
2023-05-17 22:34:52 -0500

    Second part

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import './App.css'
 function App() {
 
   //Creamos un div y dentro de el le asignamos el componente Message
-  return <div> <Message/> </div>;
+  //return <div> <Message/> </div>;
+
+  return (
+      <Message message = "Hello from app"/>
+  );
 }
 
 export default App
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,17 +1,23 @@
+interface Props{
+    message : string;
+}
+
 //Se declara una función con un retorno que es el resultado del modulo
-function Message(){
+function Message({message} : Props){
 
     //Cuando se van a usar multples lineas de HTML es necesario poner todo dentro de ( )
     //Se usa dentro del ( ) el <> y </> cuando vamos a ingresar varios tags HTML 
 
     //El siguiente ejemplo usa bootstrap para agregar la clase de estilo css con el tag test-muted. Se usa classname en lugar de class
 
-    return (
+    /*return (
         <>
             <h3> Hello world </h3>  
             <small className="text-muted"> By Santiago Trochez and Juan Prada xd</small>
         </>
-    );
+    );*/
+    
+    return <h1> {message} </h1>
 }
 
 export default Message;
