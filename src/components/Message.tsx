b627136b295df83615860c9b95374c7eee08cf0f
DaniloErazo <erazodanilo1218@gmail.com>
2023-05-28 11:02:59 -0500

    Basic login

---
+++ src/components/Message.tsx
@@ -0,0 +1,15 @@
+function Message() {
+    
+    return(
+
+    <>
+    
+    <h3>Hello world</h3>
+    <small className="text-muted">This is a message</small>
+
+
+    </>
+    );
+}
+
+export default Message
---
+++ src/components/NotFound.tsx
@@ -0,0 +1,15 @@
+function NotFound() {
+    
+    return(
+
+    <>
+    
+    <h3>NotFound</h3>
+    <small className="text-muted">404</small>
+
+
+    </>
+    );
+}
+
+export default NotFound;
